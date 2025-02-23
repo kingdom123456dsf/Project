de24ff3a403326656754e522ba52aaec57829e94
cyzk <145312501+kingdom123456dsf@users.noreply.github.com>
2025-02-23 10:21:06 +0800

    Add files via upload

--- 每日一题2025/6131.农夫约翰最喜欢的操作.cpp
+++ 每日一题2025/6131.农夫约翰最喜欢的操作.cpp
@@ -1,36 +1,41 @@
 #include<iostream>
 #include<cstdio>
+#include<algorithm>
 
 using namespace std;
 
 typedef long long LL;
 const int N = 200010;
-LL a[N];
-LL m;
-int ans,res;
+LL a[2 * N], s[2 * N];
+int t,n,m;
 
 int main(){
-	int t,n;
 	scanf("%d", &t);
 	
 	while(t--)
 	{
-		scanf("%d%lld", &n, &m);
-		for(int i = 1;i <= n;i++) scanf("%lld", &a[i]);
-		
-		res = 1e9;
-		for(int x = 0;x <= m;x++)
+		scanf("%d%d", &n, &m);
+		for(int i = 1;i <= n;i++)
 		{
-			for(int i = 1;i <= n;i++)
-			{
-			 	a[i] -= x;
-			 	int num = a[i] % m; 
-			 	if(num > (m / 2)) ans = ans + m - num;
-		 		else ans += num;
-			}
-			res = min(res, ans);
-			ans = 0;
+			scanf("%lld", &a[i]);
+			a[i] %= m;
+			a[i + n] = a[i] + m;
+		} 
+		sort(a + 1,a + 1 + n + n);//将整个数组排序(后面n个是加上m的数组)
+		
+		for(int i = 1;i <= 2 * n;i++)
+			s[i] = s[i - 1] + a[i];
+			
+		LL res = 1e18;
+		for(int i = 1;i <= n;i++)//***注意是取长度为n的区间 
+		{
+			int l = i,r = i + n - 1;
+			int mid = l + r >> 1;
+			LL sum = ((mid - l + 1) * a[mid] - (s[mid - 1] - s[l - 1])) + 
+			((s[r] - s[mid]) - (r - mid + 1) * a[mid]);
+			res = min(res, sum);
 		}
+		
 		cout << res << endl;
 	}
 	
