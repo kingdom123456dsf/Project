57410516822663bb6bc9f03260ca5e242e06fbf5
cyzk <145312501+kingdom123456dsf@users.noreply.github.com>
2025-04-06 23:18:50 +0800

    Add files via upload

--- 蓝桥杯真题/省赛/第十二届蓝桥杯C++/砝码称重.cpp
+++ 蓝桥杯真题/省赛/第十二届蓝桥杯C++/砝码称重.cpp
@@ -1,40 +1,45 @@
 #include<iostream>
+#include<cstdio>
 #include<algorithm>
 
 using namespace std;
 
-const int N = 100010;
-int a[N];
-bool st[110][N];//记录状态
-bool f[N];//记录每个总量的答案是否存在 
+const int N = 110, M = 2 * 100010;//M代表重量
+int w[N];
+bool f[N][M]; 
 int n;
-
-void dfs(int u, int sum)//第几个砝码,和答案
-{
-	if(st[u][sum]) return;//若是当前状态已经有了，剪枝
-	st[u][sum] = true;
-	
-	f[sum] = true;//该答案出现过 
-	
-	if(u == n) return;//选完了 
-	
-	dfs(u + 1, sum + a[u]);//放一边 
-	dfs(u + 1, sum);//不选
-	dfs(u + 1, abs(sum - a[u]));//放另一边
-	//由于存在答案是负数,所以加绝对值 
-} 
 
 int main()
 {
-	cin >> n;
-	for(int i = 0; i < n; i ++ ) cin >> a[i];
+	scanf("%d", &n);
 	
-	dfs(0, 0);//暴搜 
+	//个数从1~n 
+	int m;//总重量 
+	for(int i = 1; i <= n; i ++ )
+	{
+		scanf("%d", &w[i]);
+		m += w[i];
+	}
+	
+	f[0][0] = true;//0个砝码，重量为0
+	
+	for(int i = 1; i <= n; i ++ )
+	{
+		for(int j = 0; j <= m; j ++ )
+		{
+			//因为是bool 
+			f[i][j] = f[i - 1][j] || f[i - 1][abs(j - w[i])] || f[i - 1][j + w[i]];
+			//j - w[i]可能为负，故abs；j + w[i]可能超过1e5,故数据范围要*2 
+		} 
+	}
 	
 	int res = 0;
-	for(int i = 1; i < N; i ++ ) if(f[i]) res ++;
+	for(int j = 1; j <= m; j ++ )
+	{
+		if(f[n][j]) res ++;
+	}
 	
-	cout << res << endl;
-	
+	printf("%d", res);
+		
 	return 0;
 }
